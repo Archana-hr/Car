69879fc89b857ccde3d225e046f002bb8c2355f4
Archana-hr <archanatcs19@gmail.com>
2022-02-16 15:04:17 +0530

    updated

---
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.repository.CustomerRepository;
+import com.example.demo.entity.Customer;
+
+@Service
+public class CustomerService {
+	@Autowired
+	CustomerRepository customerRepository;
+
+	public Optional<Customer> getcustomer(Integer id) {
+		return customerRepository.findById(id);
+	}
+
+	public Iterable<Customer> getCustomers() {
+		return customerRepository.findAll();
+	}
+
+	public void saveCustomer(@Valid Customer customer) {
+		customerRepository.save(customer);
+	}
+}
